9e54d23f78f80b519d1a8407e4cdd0219c14309e
aomine <aomine1745@gmail.com>
2020-08-12 14:37:25 -0500

    integration of body-parser

--- backend-node/server.js
+++ backend-node/server.js
@@ -1,16 +1,26 @@
 const express = require('express')
-const router = express.Router()
+const bodyParser = require('body-parser')
 
+const router = express.Router()
 const app = express()
 
+// Body Parser
+app.use(bodyParser.json())
+
+// Router
 app.use('/api/message', router)
 
 router.get('/', (req, res) => {
   res.send('Lista de mensajes')
 })
 
-router.post('/', (req, res) => {
-  res.send('Mensaje añadido')
+router.delete('/', (req, res) => {
+  console.log(req.body)
+  console.log(req.query)
+
+  const { id } = req.body
+
+  res.send(`Mensaje con el id ${id} ha sido removido correctamente`)
 })
 
 app.listen(3000, () => {
